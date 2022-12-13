aa2678ce4a762ee03c00b6ff9b4958d8b4114f69
Elier H <elierporto@gmail.com>
2022-12-12 20:15:33 -0500

    Initialize project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Express Basics
+
+### Start a new project
+
+```
+npm init -y
+npm i express mongoose
+npm i nodemon --save-dev
+touch index.js
+```
+
+In your package.json, create a "start" script
+
+```
+"scripts": {
+    "start": "nodemon index.js"
+  },
+
+  npm start
+```
---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Todo = require("./model/Todo");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/express-basics");
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Queries "/practice?api_key=123456"
+app.get("/practice", (req, res) => {
+  const query = req.query;
+
+  res.send({ name: "practice", status: 200, api_key: query.api_key });
+});
+
+// Params "/practice/1"
+app.get("/practice/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+
+  res.send({ id });
+});
+
+// BASIC CRUD ROUTES - Create, Read, Update, Delete
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.send(todos);
+  } catch (error) {
+    res.send("Data not found");
+  }
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    res.send(todo);
+  } catch (error) {
+    res.send("Error: todo not found");
+  }
+});
+
+app.post("/todos", async (req, res) => {
+  if (req.query.name) {
+    const todo = await Todo.create({ name: req.query.name });
+    res.send(todo);
+  } else {
+    res.send("Error, no query was provided.");
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.send("successfuly deleted");
+  } catch (error) {
+    res.send("Error, failed to delete.");
+  }
+});
+
+app.patch("/todos/:id", async (req, res) => {
+  try {
+    await Todo.findByIdAndUpdate(req.params.id, {
+      name: req.query.name,
+    });
+    res.send("Successfully updated the todo item.");
+  } catch (error) {
+    res.send("Error, failed to update.");
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running in port 5000.");
+});
